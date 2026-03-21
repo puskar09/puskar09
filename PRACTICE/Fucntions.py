ab40a230681ed52c883cdae5ff214c8ef9faecc9
puskar09 <mishra.puskar05@gmail.com>
2026-03-21 21:21:19 +0530

    ai resume builder part-1

---
+++ PRACTICE/Fucntions.py
@@ -0,0 +1,7 @@
+def ashadow_clone_jutsu(chakra_level):
+    if chakra_level==0:
+        print())
+
+    
+
+    
---
+++ SCRIPTS/airesume.py
@@ -0,0 +1,5 @@
+
+with open(r'C:\Users\Puskar Mishra\OneDrive\Desktop\Puskar09\puskar09\SCRIPTS\sample_resume.txt', 'r') as f:
+    resume = f.read()
+
+print(resume)
